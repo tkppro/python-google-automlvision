d9bae88cfc0a5ad66a744dade8ade16f3c26f6e9
htv-thangdm <thangdm@hybrid-technologies.vn>
2021-03-26 18:03:48 +0700

    feature

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # python-google-automlvision
+
+#Requirements:
+`python 3.8.*`
+
+#How to run?
+``
+poetry install
+``
+
+``
+export GOOGLE_APPLICATION_CREDENTIALS=`$PATH`
+``
+
+``
+uvicorn main:app --reload --port 8003
+``
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+
+from python_automl_vision.services.services import (
+    detect_labels_uri,
+    detect_labels_file,
+    predict,
+)
 
 app = FastAPI()
 
@@ -6,3 +13,25 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/detect-uri")
+async def detect_image_url(uri: str):
+    results = detect_labels_uri(uri)
+    return results
+
+
+@app.post("/detect-file")
+async def detect_image_file(file: bytes = File(...)):
+    results = detect_labels_file(file)
+    return results
+
+
+@app.post("/predict")
+async def predict_image_file(file: bytes = File(...)):
+    results = predict(file)
+    return results
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8003)
